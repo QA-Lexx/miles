2583e34c3c32e12f2fc15cee396bdef90938a2fd
QA-Lexx <ilin_001@mail.ru>
2022-11-06 00:26:54 +0300

    add modified main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 50;
+
+        int bonus = 20;
+
+        int mile = amount / bonus;
+
+        System.out.println(mile);
+
+    }
+}
